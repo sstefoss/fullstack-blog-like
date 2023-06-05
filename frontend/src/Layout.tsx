bbe417654f2f98ddf528013a0194ed01057fe295
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 19:07:28 +0300

    feat(frontend): login/signup buttons fixed on top

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
-      Login / Signup
+      <div className="fixed top-0 right-0 p-4">
+        <Link to="/login" className="mr-4">
+          Login
+        </Link>
+        <Link to="/signup">Signup</Link>
+      </div>
       <Outlet />
     </div>
   );
