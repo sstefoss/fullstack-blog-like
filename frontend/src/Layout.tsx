2e192b939c89859d6041c4a004ca6595a8bf6815
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 22:24:06 +0300

    feat(frontend/nav): render different links for loggedin user

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,7 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import { HomeIcon } from "@heroicons/react/solid";
+import { isLoggedIn } from "./utils";
 
 const Layout = () => {
+  const links = isLoggedIn()
+    ? [
+        { to: "/profile", label: "Profile" },
+        { to: "/logout", label: "Logout" },
+      ]
+    : [
+        { to: "/signup", label: "Signup" },
+        { to: "/login", label: "Login" },
+      ];
+
   return (
     <div>
       <div className="flex fixed top-0 w-full px-6 py-4 justify-between bg-gray-900 border-b border-gray-700">
@@ -9,11 +20,8 @@ const Layout = () => {
           <HomeIcon className="h-6 w-6 text-gray-400 hover:text-gray-200 hover:cursor-pointer" />
         </Link>
         <div>
-          {[
-            { to: "/signup", label: "Signup" },
-            { to: "/login", label: "Login" },
-          ].map((l) => (
-            <NavLink to={l.to} label={l.label} />
+          {links.map((l, i) => (
+            <NavLink key={i} to={l.to} label={l.label} />
           ))}
         </div>
       </div>
