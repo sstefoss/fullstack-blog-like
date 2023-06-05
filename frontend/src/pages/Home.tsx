9b231c656fbbb5f0b0970bcb3bd55bec1f13db60
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 20:52:58 +0300

    feat(frontend): cleanup home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,21 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { PostInterface } from "../interfaces.tsx";
+import { LIST_POSTS } from "../gql/post.ts";
 
 import Post from "../components/Post.tsx";
 
-const GET_POSTS = gql`
-  query GetPosts {
-    posts {
-      id
-      title
-      body
-    }
-  }
-`;
-
-const Home = () => {
-  const { loading, error, data } = useQuery(GET_POSTS);
+const HomePage = () => {
+  const { loading, error, data } = useQuery(LIST_POSTS);
 
   if (loading) return <div>Loading</div>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.posts.map((p: PostInterface) => <Post post={p} />);
+  return (
+    <div className="flex flex-col items-center">
+      {data.posts.map((p: PostInterface) => (
+        <Post post={p} />
+      ))}
+    </div>
+  );
 };
-export default Home;
+export default HomePage;
