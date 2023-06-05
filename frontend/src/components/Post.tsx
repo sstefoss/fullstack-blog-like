6cbeadbe479fe3c1e63f85ee65cf7463d2ad14ee
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 20:53:33 +0300

    feat(frontend): created `Post` component

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 import { PostInterface } from "../interfaces.tsx";
 
 const Post = ({ post: { id, title, body } }: { post: PostInterface }) => {
   return (
     <div key={id} className="mb-4">
-      <h3 className="font-bold">{title}</h3>
-      <p>{body}</p>
+      <Link
+        to={`/posts/${id}`}
+        className="flex block items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <div className="flex flex-col justify-between px-6 py-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {body}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 };
