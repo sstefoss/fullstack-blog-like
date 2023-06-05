8cff68234349b973b93c675e884f955fd73f8495
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 21:55:08 +0300

    feat(frontend/post): add reaction buttons

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,7 +1,15 @@
-import { Link } from "react-router-dom";
+import { ThumbUpIcon, ThumbDownIcon } from "@heroicons/react/solid";
+import { Tooltip } from "react-tooltip";
 import { PostInterface } from "../interfaces.tsx";
 
 const Post = ({ post: { id, title, body } }: { post: PostInterface }) => {
+  const like = (e) => {
+    e.stopPropagation();
+  };
+
+  const dislike = (e) => {
+    e.stopPropagation();
+  };
   return (
     <div key={id} className="mb-4">
       <Link
@@ -15,6 +23,23 @@ const Post = ({ post: { id, title, body } }: { post: PostInterface }) => {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {body}
           </p>
+          <div className="flex items-center justify-end">
+            <div
+              className="flex items-center"
+              data-tooltip-id="auth-warning"
+              data-tooltip-content="Reactions are disabled for non-logged-in users."
+            >
+              <ThumbUpIcon
+                onClick={like}
+                className="h-6 w-6 text-gray-400 hover:text-gray-200 hover:text-green-600 hover:cursor-not-allowed mr-2"
+              />
+              <ThumbDownIcon
+                onClick={dislike}
+                className="h-6 w-6 text-gray-400 hover:text-gray-200 hover:text-red-600 hover:cursor-pointer"
+              />
+              <Tooltip id="auth-warning" />
+            </div>
+          </div>
         </div>
       </Link>
     </div>
