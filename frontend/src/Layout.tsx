26b92289f42c0d9075fe7fbf7e58593f26f0b6d6
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 20:51:54 +0300

    feat(frontend): improved layout styling

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,16 +1,35 @@
 import { Link, Outlet } from "react-router-dom";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Layout = () => {
   return (
-    <div>
-      <div className="fixed top-0 right-0 p-4">
-        <Link to="/login" className="mr-4">
-          Login
-        </Link>
-        <Link to="/signup">Signup</Link>
+    <div className="bg-gray-900">
+      <div className="flex fixed top-0 w-full px-6 py-4 justify-between bg-gray-900 border-b border-gray-700">
+        <HomeIcon className="h-6 w-6 text-gray-400 hover:text-gray-200 hover:cursor-pointer" />
+        <div>
+          {[
+            { to: "/signup", label: "Signup" },
+            { to: "/login", label: "Login" },
+          ].map((l) => (
+            <NavLink to={l.to} label={l.label} />
+          ))}
+        </div>
       </div>
-      <Outlet />
+      <div className="pt-28">
+        <Outlet />
+      </div>
     </div>
+  );
+};
+
+const NavLink = ({ to, label }: { to: string; label: string }) => {
+  return (
+    <Link
+      to={to}
+      className="ml-4 text-gray-400 hover:text-gray-200 transition-colors"
+    >
+      {label}
+    </Link>
   );
 };
 
