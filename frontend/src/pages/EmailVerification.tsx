1aa5401fd96b15628801bb4050ca6262c8445f15
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 19:04:55 +0300

    feat(frontend): token verification page

--- frontend/src/pages/EmailVerification.tsx
+++ frontend/src/pages/EmailVerification.tsx
@@ -1,29 +1,30 @@
 import { useEffect } from "react";
 import { useMutation } from "@apollo/client";
-import { redirect, useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { VERIFY_EMAIL } from "../gql/user";
 
 const EmailVerification = () => {
-  const [verifyEmail, { error }] = useMutation(VERIFY_EMAIL);
+  const [verifyEmail, { error, data }] = useMutation(VERIFY_EMAIL);
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const { email, token } = searchParams;
-    verifyEmail({ variables: { email, token } })
-      .then((data) => {
-        localStorage.setItem("token", data.data.verifyEmail.token);
-        setTimeout(() => {
-          redirect("/");
-        }, 3000);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    const email = searchParams.get("email");
+    const token = searchParams.get("token");
+    verifyEmail({ variables: { email, token } });
   }, [searchParams, verifyEmail]);
 
   if (error && error.message === "no_such_token") {
     return <div>No such token</div>;
   }
+
+  if (data?.verifyEmail) {
+    localStorage.setItem("token", data.verifyEmail.token);
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  }
+
   return <div>Verifying email...</div>;
 };
 
