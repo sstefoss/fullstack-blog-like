9e2c0100f22c0acd3ce3dc0e675342341cb90f79
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-05 20:26:13 +0300

    feat(frontend): improved email verification styling

--- frontend/src/pages/EmailVerification.tsx
+++ frontend/src/pages/EmailVerification.tsx
@@ -3,12 +3,13 @@ import { useMutation } from "@apollo/client";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { VERIFY_EMAIL } from "../gql/user";
 
-const EmailVerification = () => {
+const EmailVerificationPage = () => {
   const [verifyEmail, { error, data }] = useMutation(VERIFY_EMAIL);
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log(searchParams);
     const email = searchParams.get("email");
     const token = searchParams.get("token");
     verifyEmail({ variables: { email, token } });
@@ -25,7 +26,19 @@ const EmailVerification = () => {
     }, 3000);
   }
 
-  return <div>Verifying email...</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-1/3 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-6 text-3xl font-bold text-gray-900 dark:text-white">
+          Verifying email...
+        </h5>
+        <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+          Sit tight while we confirm your email address, we'll have you up and
+          running in no time!
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default EmailVerification;
+export default EmailVerificationPage;
