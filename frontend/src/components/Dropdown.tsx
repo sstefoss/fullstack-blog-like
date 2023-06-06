38ab4074f833526379d88d0bc72070cdd7efa999
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-06 19:57:56 +0300

    fix(frontend): remove unused imports

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
 import cn from "classnames";
 
