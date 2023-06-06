a75286d2329e6a9cc79963b99f8ef114893fe2ec
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-06 22:25:56 +0300

    feat(frontend): minor styling updates in navbar

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { HomeIcon } from "@heroicons/react/solid";
 import { AuthContext } from "./context/auth";
+import cn from "classnames";
 
 const Layout = () => {
   const { loggedIn } = useContext(AuthContext);
+  const location = useLocation();
   const links = loggedIn
     ? [
         { to: "/profile", label: "Profile" },
@@ -15,15 +17,26 @@ const Layout = () => {
         { to: "/login", label: "Login" },
       ];
 
+  const isHomePageActive = () => location.pathname === "/";
   return (
     <div>
       <div className="flex fixed top-0 w-full px-6 py-4 justify-between bg-gray-900 border-b border-gray-700">
         <Link to="/">
-          <HomeIcon className="h-6 w-6 text-gray-400 hover:text-gray-200 hover:cursor-pointer" />
+          <HomeIcon
+            className={cn("h-6 w-6 hover:text-gray-200 hover:cursor-pointer", {
+              "text-gray-400": !isHomePageActive(),
+              "text-gray-100": isHomePageActive(),
+            })}
+          />
         </Link>
         <div>
           {links.map((l, i) => (
-            <NavLink key={i} to={l.to} label={l.label} />
+            <NavLink
+              active={location.pathname === l.to}
+              key={i}
+              to={l.to}
+              label={l.label}
+            />
           ))}
         </div>
       </div>
@@ -32,15 +45,22 @@ const Layout = () => {
   );
 };
 
-const NavLink = ({ to, label }: { to: string; label: string }) => {
-  return (
-    <Link
-      to={to}
-      className="ml-4 text-gray-400 hover:text-gray-200 transition-colors"
-    >
-      {label}
-    </Link>
-  );
-};
+interface NavLinkProps {
+  to: string;
+  label: string;
+  active: boolean;
+}
+
+const NavLink = ({ to, label, active }: NavLinkProps) => (
+  <Link
+    to={to}
+    className={cn("ml-4 hover:text-gray-200 transition-colors font-semibold", {
+      "text-gray-100": active,
+      "text-gray-400": !active,
+    })}
+  >
+    {label}
+  </Link>
+);
 
 export default Layout;
