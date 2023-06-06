3d3cfd089444f1f7fc7eff3993d8fc15a669c929
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-06 16:35:39 +0300

    feat(frontend/gql): fetch user reacted posts

--- frontend/src/gql/user.ts
+++ frontend/src/gql/user.ts
@@ -1,5 +1,5 @@
 import { gql } from "@apollo/client";
-import { USER_FRAGMENT } from "./fragments";
+import { REACTION_FRAGMENT, POST_FRAGMENT, USER_FRAGMENT } from "./fragments";
 
 export const ME = gql`
   query Me {
@@ -10,6 +10,25 @@ export const ME = gql`
     }
   }
   ${USER_FRAGMENT}
+`;
+
+export const MY_POSTS = gql`
+  query {
+    me {
+      User {
+        reactions {
+          post {
+            ...post
+            reactions {
+              ...reaction
+            }
+          }
+        }
+      }
+    }
+  }
+  ${POST_FRAGMENT}
+  ${REACTION_FRAGMENT}
 `;
 
 export const VERIFY_EMAIL = gql`
